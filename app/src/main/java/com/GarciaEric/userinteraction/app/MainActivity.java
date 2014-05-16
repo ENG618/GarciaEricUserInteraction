ed73cdd625242b6e9cac134727e968a9c6525e97
eng618 <eng618@fullsail.edu>
2014-05-16 01:18:52 -0400

    Minor updates to layout.  Completed spinner

--- app/src/main/java/com/GarciaEric/userinteraction/app/MainActivity.java
+++ app/src/main/java/com/GarciaEric/userinteraction/app/MainActivity.java
@@ -6,32 +6,119 @@
 package com.GarciaEric.userinteraction.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
     private static final String LOGTAG = "MainActivity";
+    private Context mContext;
+    private String[] coursesArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Log message
         Log.d(LOGTAG, "onCreate entered");
 
+        // Set up spinner
+        mContext = this;
+        coursesArray = getResources().getStringArray(R.array.courses_array);
+        setSpinner();
+
+        // Create button listeners
+        //setButtons();
 
     }
 
-    //
-    public void getRecipe(String searchTerm){
+    private void setSpinner() {
+        // Log message
+        Log.d(LOGTAG, "setSpinner entered");
+
+        // Obtain spinner
+        Spinner courseSpinner = (Spinner) findViewById(R.id.courseSpinner);
+
+        // Spinner adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, coursesArray);
+
+        // Specify layout
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply adapter to spinner
+        courseSpinner.setAdapter(adapter);
+
+        courseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Toast item selected
+                Toast.makeText(mContext, "You have selected: " + coursesArray[position], Toast.LENGTH_LONG).show();
+
+                // Temp log
+                Log.d(LOGTAG, "An item was selected from the spinner");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    // Get recipies method
+    private void getRecipe(String searchTerm){
         // Log message
         Log.d(LOGTAG, "getRecipe entered");
     }
 
+    private void setButtons(){
+
+        // Obtain buttons
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // Event for search
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log message
+                Log.d(LOGTAG, "Search button pressed");
+            }
+        });
+
+        // Event for cancel
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log message
+                Log.d(LOGTAG, "Cancel button pressed");
+            }
+        });
+    }
+
+    // Cancel button action
+    public void onCancel(View v){
+        // Log message
+        Log.d(LOGTAG, "Cancel button clicked");
+    }
+
+    // Save button action
+    public void onSearch(View v){
+        // Log message
+        Log.d(LOGTAG, "Save button clicked");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
