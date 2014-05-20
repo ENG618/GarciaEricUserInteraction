3db336f98ad328232fb58e7fead68551178ecdb2
eng618 <eng618@fullsail.edu>
2014-05-20 15:17:55 -0400

    Created custom constructor class for Recipe.

---
+++ app/src/main/java/com/GarciaEric/userinteraction/Data/Recipe.java
@@ -0,0 +1,28 @@
+package com.GarciaEric.userinteraction.Data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by: ENG618-Mac on 5/20/14
+ * Full Sail University
+ */
+
+public class Recipe {
+    // Recipe variables
+    public String recipeID;
+    public String recipeName;
+    public String sourceDisplayName;
+    public ArrayList<String> ingredients;
+    public int totalTimeInSeconds;
+    public String recipeURL;
+
+    // Construct recipe object
+    public Recipe (String recipeName, String recipeID, String sourceDisplayName, ArrayList<String> ingredients, int totalTimeInSeconds, String recipeURL) {
+        this.recipeName = recipeName;
+        this.recipeID = recipeID;
+        this.sourceDisplayName = sourceDisplayName;
+        this.ingredients = ingredients;
+        this.totalTimeInSeconds = totalTimeInSeconds;
+        this.recipeURL = recipeURL;
+    }
+}
---
+++ app/src/main/java/com/GarciaEric/userinteraction/Data/RecipeData.java
@@ -0,0 +1,8 @@
+package com.GarciaEric.userinteraction.Data;
+
+/**
+ * Created by: ENG618-Mac on 5/20/14
+ * Full Sail University
+ */
+public class RecipeData {
+}
